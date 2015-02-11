fcd52fee6b58aa93564231887e2917537eec365c
eudemonics <vvn@eudemonics.org>
2015-02-10 20:54:37 -0800

    added automatic installation of spoof-mac if not found

--- flushwifi.sh
+++ flushwifi.sh
@@ -8,6 +8,31 @@ if (( $(id -u) )) ; then
   exit 1
 fi
 
+echo ""
+echo "************************************"
+echo "FLUSHWIFI.SH by vvn<vvn@notworth.it>"
+echo "************************************"
+echo ""
+echo "checking if spoof-mac is installed.."
+echo ""
+
+if ! type "spoof-mac" > /dev/null; then
+  git clone https://github.com/feross/spoofmac spoofmac
+  cd spoofmac
+  python setup.py install
+  if type "spoof-mac" > /dev/null; then
+    echo "spoof-mac successfully installed!"
+    cd ..
+    rm -rf spoof-mac
+  else
+    echo "there was a problem installing spoof-mac. please check you have python and easy_install properly installed."
+  fi
+else
+  SPOOFMAC=`which spoof-mac`
+  echo "spoof-mac found at $SPOOFMAC, no need to install!"
+fi
+
+echo ""
 echo "refreshing wifi services.."
 sysctl -w net.inet.icmp.drop_redirect=1
 arp -a -d
@@ -55,4 +80,3 @@ sleep 2
 echo "Wi-Fi successfully restarted."
 
 exit 0
-
